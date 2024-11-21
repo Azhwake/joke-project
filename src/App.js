570f49ad0772d451380eba7157abc2eb359b2a6a
Azhwake <azhwaq2002@gmail.com>
2024-11-21 14:59:34 +0300

    DONE

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import TwoPart from "./TwoPart";
 import SingleJoke from "./SingleJoke";
+import { useState,useEffect } from "react";
 
 function App() {
+  const [joke,setjoke]= useState({});
+  function getjoke () {
+    fetch("https://v2.jokeapi.dev/joke/Programming")
+    .then((res)=>{
+      return res.json()
+    })
+    .then((data)=>{
+      setjoke(data)
+      console.log(data)
+    })
+  }
+  useEffect(()=> {
+    getjoke();
+  }, []);
   return (
     <div className="app">
       <h2 className="heading">😂 Jokes! 😂</h2>
-      <SingleJoke />
-      <TwoPart />
+      {joke.type==="single"? <SingleJoke data={joke} /> : <TwoPart data={joke} />}
 
       <div className="buttons">
-        <button className="another">Another Joke!😆</button>
+        <button className="another"onClick={getjoke}>Another Joke!😆</button>
       </div>
     </div>
   );
